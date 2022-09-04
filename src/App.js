a27e1f7983646299d108a96504eb6b142dcdcc0f
OutliersAdvantage <102266508+LakelonBailey@users.noreply.github.com>
2022-09-04 17:26:42 -0400

    added App.js

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { useState } from 'react';
+import Nav from './components/Nav';
+import About from './components/About';
+import ContactForm from './components/Contact';
+
+function App() {
+  return (
+    <div>
+      <main>
+          <About></About>
+          <ContactForm></ContactForm>
+      </main>
+    </div>
+  );
+}
+
+export default App;
