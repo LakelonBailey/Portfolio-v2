4495c0015e645bad684f8f62fd15c02bde5c782b
LakelonBailey <lake.bailey@icloud.com>
2023-10-30 13:35:16 -0400

    adjusted work experience

--- src/data/workExperiences.js
+++ src/data/workExperiences.js
@@ -3,15 +3,11 @@ const workExperiences = [
         company: 'IBM',
         positions: [
             {
-                title: 'Backend Development Intern',
-                duration: 'May 2023 - Present',
+                title: 'Backend Development Co-op',
+                duration: 'August 2023 - Present',
                 notes: [
-                    "Contributing to the strategic re-architecture of a global Vue.js/Spring Boot application, serving approximately 240,000 device-related requests annually from IBM employees, new hires, vendors, and contractors.",
-                    "Leading a pivotal role in the transformation of the application from a monolithic design to a microservices-based structure, enabling more scalable, maintainable, and resilient system performance.",
-                    "Refining and implementing modern authentication and authorization mechanisms, significantly improving system security and user experience by ensuring the right access controls for various user roles.",
-                    "Collaborating closely with a dynamic team, adapting to agile methodologies, and utilizing problem-solving skills to navigate complex software development challenges.",
-                    "Reduced on-page-load API calls by 68% by removing unnecessary data retrieval."
-
+                    "Working alongside the data scientists in the Devices Data Insights team to develop efficient data-processing solutions.",
+                    "Developing a robust pipeline to rapidly transport millions of records from ServiceNow to IBM Cloud Object Storage."
                 ]
             }
         ]
@@ -20,13 +16,29 @@ const workExperiences = [
         company: 'ACTprep.com, Inc.',
         positions: [
             {
-                title: 'Chief Technology Officer',
+                title: 'Software Engineer',
                 duration: 'March 2023 - Present',
                 notes: [
                     "Managing the technology division of ACTprep.com.",
                     "Leading the development and maintenance of the ACTprep.com web application: an application designed to receive and process all company data for the purpose of coaching, analytics, student progress monitoring, and more. This application serves approximately 100 daily users and involves over 30 unique database tables.",
                     "Improving scale of the company by increasing the total possible students a coach can mentor by over 100%.",
                     "Consistently decreasing company costs, having reduced annual labor/supplies costs by over $50,000."
+                ]
+            }
+        ]
+    },
+    {
+        company: 'IBM',
+        positions: [
+            {
+                title: 'Backend Development Intern',
+                duration: 'May 2023 - August 2023',
+                notes: [
+                    "Contributed to the strategic re-architecture of a global Vue.js/Spring Boot application that facilitates over 315k users, including all IBM employees.",
+                    "Lead a pivotal role in the transformation of the application from a monolithic design to a microservices-based structure.",
+                    "Refined and implemented modern authentication and authorization mechanisms, significantly improving system security and user experience by ensuring the right access controls for various user roles.",
+                    "Collaborated closely with a dynamic team and adapting to agile methodologies.",
+                    "Reduced on-page-load API calls by 68% by re-organizing and optimizing data retrieval."
                 ]
             }
         ]
@@ -50,7 +62,7 @@ const workExperiences = [
         company: 'ACTprep.com, Inc.',
         positions: [
             {
-                title: 'Contractual Application Developer',
+                title: 'Software Developer',
                 duration: 'May 2022 - August 2022',
                 notes: [
                     "Developed a full-stack web application that utilizes the Google Sheets API to store student enrollment data provided by new students as they traverse through the application’s enrollment process. ",
